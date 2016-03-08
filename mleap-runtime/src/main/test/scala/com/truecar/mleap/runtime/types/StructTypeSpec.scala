fddc23d510e2a0bff763f1f9a79750c75317a0c5
Prianna Ahsan <pahsan@truecar.com>
2016-03-08 15:09:40 -0800

    Update runtime tests: Began adding StructType tests.

---
+++ mleap-runtime/src/main/test/scala/com/truecar/mleap/runtime/types/StructTypeSpec.scala
@@ -0,0 +1,33 @@
+package com.truecar.mleap.runtime.types
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by pahsan on 3/8/16.
+  */
+class StructTypeSpec extends FunSuite{
+  val fields = Seq(StructField("first", DataType.fromName("string")),
+                   StructField("second", DataType.fromName("string")),
+                   StructField("third", DataType.fromName("string")),
+                   StructField("fourth", DataType.fromName("string")),
+                   StructField("fifth", DataType.fromName("string"))
+  )
+
+  val testStruct = StructType(fields)
+
+  test("getField should return the desired field wrapped in an Option"){
+    assert(testStruct.getField("first").get.name == "first")
+  }
+
+  test("indexOf should return the integer index of the desired field"){
+    assert(testStruct.indexOf("first") == 0)
+  }
+
+  test("getIndexOf should return the same thing as indexOf, wrapped in an Option"){
+    assert(testStruct.getIndexOf("first").get == testStruct.indexOf("first"))
+  }
+
+  test("Dropping a field from a StructType should remove the index nameToIndex"){
+
+  }
+}
