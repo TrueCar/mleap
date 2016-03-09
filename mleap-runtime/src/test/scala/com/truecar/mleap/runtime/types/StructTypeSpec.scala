d5e6d3d387ae303df0103c613ec5dc3a99bb6900
Prianna Ahsan <pahsan@truecar.com>
2016-03-08 16:55:25 -0800

    Update StructType: Added more tests.

--- mleap-runtime/src/test/scala/com/truecar/mleap/runtime/types/StructTypeSpec.scala
+++ mleap-runtime/src/test/scala/com/truecar/mleap/runtime/types/StructTypeSpec.scala
@@ -1,11 +1,11 @@
 package com.truecar.mleap.runtime.types
 
-import org.scalatest.{GivenWhenThen, FunSpec, FunSuite}
+import org.scalatest.{TryValues, GivenWhenThen, FunSuite}
 
 /**
   * Created by pahsan on 3/8/16.
   */
-class StructTypeSpec extends FunSuite with GivenWhenThen{
+class StructTypeSpec extends FunSuite with GivenWhenThen with TryValues{
   val fields = Seq(StructField("first", DataType.fromName("string")),
                    StructField("second", DataType.fromName("string")),
                    StructField("third", DataType.fromName("string")),
@@ -27,7 +27,37 @@ class StructTypeSpec extends FunSuite with GivenWhenThen{
     assert(testStruct.getIndexOf("first").get == testStruct.indexOf("first"))
   }
 
+  test("The contains operation should return true whenever a field exists"){
+    assert(fields.map(f => testStruct.contains(f.name)).forall(identity))
+  }
+
+  test("The contains operation should return false when a field doesn't exist"){
+    val fieldsPrime = fields:+StructField("sixth", DataType.fromName("string"))
+
+    assert(!fieldsPrime.map(f => testStruct.contains(f.name)).forall(identity))
+  }
+
+  test("withField should return a StructType with the field added"){
+    val field = StructField("sixth", DataType.fromName("string"))
+
+    assert(testStruct.withField(field).contains(field.name))
+  }
+
   test("Dropping a field from a StructType should remove the field"){
     assert(testStruct.dropField("first").get.getField("first").isEmpty)
+  }
+
+  test("select should return a StructType with selected fields"){
+    Given("an array of valid String names")
+    val selection = Array("first", "second", "third")
+
+    When("a selection is made")
+    val selectedFields = testStruct.select(selection:_*)
+
+    Then("the operation should return a success")
+    assert(selectedFields.isSuccess)
+
+    And("the StructType should contain the selected fields")
+    assert(selection.map(f => selectedFields.success.value.contains(f)).forall(identity))
   }
 }
