3bebd30e9810738de6c3d0ce2a712fd7080079d4
prianna <prianna@g.ucla.edu>
2016-03-15 16:48:09 -0700

    LogisticRegression renamed to BinaryLogisticRegression, margin and score are now methods.

--- mleap-core/src/main/scala/com/truecar/mleap/core/classification/BinaryLogisticRegression.scala
+++ mleap-core/src/main/scala/com/truecar/mleap/core/classification/BinaryLogisticRegression.scala
@@ -4,27 +4,25 @@ import com.truecar.mleap.core.linalg.{BLAS, Vector}
 
 
 /**
-  * Created by pahsan on 3/9/16.
+  * Binary Logistic Regression
   */
-case class LogisticRegression(coefficients: Vector,
+case class BinaryLogisticRegression(coefficients: Vector,
                               intercept: Double,
-                              numClasses: Int = 2,
                               threshold: Double = 0.5) extends Serializable{
 
   val numFeatures = coefficients.size
+  val numClasses = 2
 
-  /** Computes the mean of the response variable given the predictors.
-    * For binary classification only */
-  private val margin: Vector => Double = (features) => {
+  /** Computes the mean of the response variable given the predictors. */
+  private def margin(features: Vector): Double = {
     BLAS.dot(features, coefficients) + intercept
   }
 
   /**
     * Computes the logit function over the given predictors, assuming a
     * binomial distribution.
-    * For binary classification only.
     */
-  private val score: Vector => Double = (features) => {
+  private def score(features: Vector): Double = {
     val m = margin(features)
     1.0 / (1.0 + math.exp(-m)) // Compute the logit function over the given predictors.
   }
