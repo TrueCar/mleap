3c95894c4d17c09271178e132793d2939ddc14f0
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-03-30 17:02:08 -0400

    Use val instead of def for stringToIndex.

--- mleap-core/src/main/scala/com/truecar/mleap/core/feature/StringIndexer.scala
+++ mleap-core/src/main/scala/com/truecar/mleap/core/feature/StringIndexer.scala
@@ -4,7 +4,7 @@ package com.truecar.mleap.core.feature
  * Created by hwilkins on 11/5/15.
  */
 case class StringIndexer(strings: Seq[String]) extends Serializable {
-  def stringToIndex: Map[String, Int] = strings.zipWithIndex.toMap
+  val stringToIndex: Map[String, Int] = strings.zipWithIndex.toMap
 
   def apply(value: String): Double = stringToIndex(value)
 }
